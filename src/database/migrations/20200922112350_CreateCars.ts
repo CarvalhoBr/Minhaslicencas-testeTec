675e6af487917454571fa5734400a16ea5bfa926
brandon <brandonsouza27@gmail.com>
2020-09-22 13:55:53 -0300

    Created first table

---
+++ src/database/migrations/20200922112350_CreateCars.ts
@@ -0,0 +1,21 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('carros', (table) => {
+        table.string('id').primary();
+        table.string('veiculo').notNullable();
+        table.string('marca').notNullable();
+        table.integer('ano').notNullable();
+        table.text('descricao').notNullable();
+        table.boolean('vendido').defaultTo(false).notNullable();
+        table.dateTime('created', {precision: 6}).defaultTo(knex.fn.now(6)).notNullable();
+        table.dateTime('updated', {precision: 6}).defaultTo(knex.fn.now(6)).notNullable();
+      });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return await knex.schema.dropTable('carros')
+}
+
