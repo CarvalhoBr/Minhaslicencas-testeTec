2441d3396be3235ad1273a5bc18324423c81d036
brandon <brandonsouza27@gmail.com>
2020-09-23 17:14:02 -0300

    Adicionado validação dos dados em todas as rotas

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -1,88 +1,169 @@
 import {Request, response, Response} from 'express'
 import db from '../database/connection'
+import * as yup from 'yup'
 
 export async function read(req: Request, res: Response){
-    const cars = await db.select('*').from('carros')
-    return res.json(cars);
+
+    db('carros')
+    .then((carros) => res.json(carros))
+    .catch(err => res.status(500).json({ERROR: "Não foi possível processar sua solicitação"}))
+
 } 
 
-export async function find(req: Request, res: Response){
-    let { search } = req.query
+export function find(req: Request, res: Response){
+    
+    const search = String(req.query.search)
+    const searchSchema = yup.string().required()
 
-    console.log(search)
-    // Verificando se a busca é por ano
-    if(Number(search)){
-        const carros = await db.select().from('carros').where('ano', '=', Number(search))
-        return res.json(carros)
-    }
+    return searchSchema
+    .validate(search)
+    .then(() => {
+        if(Number(search)){
+            db('carros')
+            .where('ano', '=', Number(search))
+            .then((carros) => {
+                
+                if(carros.length !== 0){
+                    return res.json(carros)
+                }
 
-    const carros = await db
-        .select()
-        .from('carros')
+            })
+            .catch(err => res.status(500).json({ ERROR: "Não foi possível processar sua solicitação" }))
+        }
+
+        db('carros')
         .where('marca', '~*', String(search))
         .orWhere('veiculo', '~*', String(search))
-
-    return res.json(carros)
-
+        .then((carros) => res.json(carros))
+        .catch(err => res.status(500).json({ ERROR: "Não foi possível processar sua solicitação" }))
+    })
+    .catch(err => res.status(400).json({ERROR: "Insira um valor válido na busca"}))    
 } 
 
-export async function readById(req: Request, res: Response){
-    const { id } = req.params
+export function readById(req: Request, res: Response){
+    
+    const id = Number(req.params.id)
+    const idSchema = yup.number().required()
 
-    const carro = await db.select().from('carros').where('id', '=', id)
-    return res.json(carro)
+    return idSchema
+    .validate({ id })
+    .then(() => {
+        db('carros')
+        .where('id', '=', id)
+        .then((selected) => {
+            return res.json(selected)
+        })
+        .catch(err => res.status(500).json({ERROR: "Não foi possível processar sua solicitação"}))
+    })
+    .catch(err => res.status(400).json({ERROR: err.message}))
 } 
 
-export async function create(req: Request, res: Response){
+export function create(req: Request, res: Response){
     
     const carro = req.body
 
-    try {
-        await db('carros').insert(carro)
-        return res.send("Carro cadastrado com sucesso!")
-    } catch (error) {
-        return res.status(500).json({ERRO: "Ocorreu um erro ao inserir o dado"})
-    }
+    const carSchema = yup.object().shape({
+        veiculo: yup.string().required(),
+        marca: yup.string().required(),
+        ano: yup.number().required(),
+        descricao: yup.string(),
+        vendido: yup.boolean().default(false)
+    }).noUnknown()
+    
+    return carSchema
+    .validate(carro, { stripUnknown: false })
+    .then(() =>
+        db("carros")
+            .insert(carro)
+            .returning('id')
+            .then((id) => res.status(201).json({"Carro cadastrado com sucesso": {id}}))
+            .catch((err) =>
+                res.status(500).json({ error: "Ocorreu um erro ao inserir o dado" })
+            )
+    ).catch((error) => res.status(400).json(error));
 
 } 
 
-export async function update(req: Request, res: Response){
+export function update(req: Request, res: Response){
     const carro = req.body
-    const { id } = req.params
-    
-    try {
-        const modified = await db('carros').update(carro).where('id', '=', Number(id)).returning('*')
-        return res.json(modified)
-    } catch (error) {
-        console.error({ERRO: error.message})
-    }
+    const id = Number(req.params.id)
+
+    const carSchema = yup.object().shape({
+        veiculo: yup.string().required(),
+        marca: yup.string().required(),
+        ano: yup.number().required(),
+        descricao: yup.string(),
+        vendido: yup.boolean().default(false)
+    }).noUnknown()
+
+    const idSchema = yup.number().required()
+
+    return idSchema
+    .validate(id)
+    .then(() => {
+        return carSchema
+        .validate(carro)
+        .then(() => {
+        db('carros')
+            .update(carro)
+            .where('id', '=', Number(id))
+            .returning('*')
+            .then(modified => res.json(modified))
+            .catch(err => res.status(500).json(err.message))
+        })
+        .catch(err => res.status(400).json({ERROR: err.message}))
+    })
+    .catch(err => res.status(400).json({ERROR: "Insira um id válido"}))
 
 } 
 
 export async function edit(req: Request, res: Response){
     const carro = req.body
-    const { id } = req.params
+    const id = Number(req.params.id)
+
+    const carSchema = yup.object().shape({
+        veiculo: yup.string(),
+        marca: yup.string(),
+        ano: yup.number(),
+        descricao: yup.string(),
+        vendido: yup.boolean().default(false)
+    }).noUnknown()
+
+    const idSchema = yup.number().required()
+
+    return idSchema
+    .validate(id)
+    .then(() => {
+        return carSchema
+        .validate(carro)
+        .then(() => {
+        db('carros')
+            .update(carro)
+            .where('id', '=', Number(id))
+            .returning('*')
+            .then(modified => res.json(modified))
+            .catch(err => res.status(500).json(err.message))
+        })
+        .catch(err => res.status(400).json({ERROR: err.message}))
+    })
+    .catch(err => res.status(400).json({ERROR: "Insira um id válido"}))
+
     
-    try {
-        const modified = await db('carros').update(carro).where('id', '=', Number(id)).returning('*')
-        return res.json(modified)
-    } catch (error) {
-        console.error({ERRO: error.message})
-    }
 } 
 
 export async function remove(req: Request, res: Response){
-    const { id } = req.params
+    const id = Number(req.params.id)
 
+    const idSchema = yup.number().required()
 
-    try {
-        const deleted = await db('carros').delete('*').where('id', '=', Number(id))
-
-        return res.json(deleted)
-    } catch (error) {
-        console.error(error.message)
-
-        return res.status(500).json({ERROR: "Não foi possível processas a solicitação"})
-    }
+    return idSchema
+    .validate(id)
+    .then(() => {
+        db('carros')
+            .delete('*')
+            .where('id', '=', id)
+        .then(() => res.status(204).end())
+        .catch(err => res.status(500).json({ ERROR: "Não foi possível processar sua solicitação" }))
+    }).catch(err => res.status(400).json({ERROR: "Insira um id válido"}))
 
 } 
