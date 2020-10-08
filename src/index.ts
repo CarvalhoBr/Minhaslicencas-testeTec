bd93e4c593781f03e8dd4eaea837a194c6bf1caf
brandon <brandonsouza27@gmail.com>
2020-10-08 11:43:06 -0300

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes'
+import cors from 'cors'
 
 const app = express();
 const port = 3333;
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
